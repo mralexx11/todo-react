be4a83fabef22e50a824c584220fde6030e118a1
Alex <42402960+mralexx11@users.noreply.github.com>
2020-03-25 20:42:58 +0200

    refactoring (item-status-filter) func -> class

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './todo-list-item.css';
 
-class TodoListItem extends React.Component {
+class TodoListItem extends Component {
 
     render() {
         const { label, important = false} = this.props;
